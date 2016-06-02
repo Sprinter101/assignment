dd22dffea23900799d53e39aaccd63f7e9beb55e
Arzach <ruslan--47@yandex.ru>
2016-06-03 02:46:19 +0300

    Added more examples + modified classes names

--- design/js/script.js
+++ design/js/script.js
@@ -1,12 +1,12 @@
 var clickHandler = function(event) {
 	var block = event.target;
-	var container = document.querySelector('.js-container')
+	var container = document.querySelector('.js-cont')
 	block.style.position = "absolute";
 	block.style.margin = 0;
 	block.style.left = (container.offsetWidth / 2) - (block.offsetWidth / 2) + "px"; 
 	block.style.top = (container.offsetHeight / 2) - (block.offsetHeight / 2) + "px";
 };
 
-document.querySelector('.js-element').addEventListener('click', clickHandler);
+document.querySelector('.js-el').addEventListener('click', clickHandler);
 
 //210-104 106
